225549c4e7c1a0782ee71fe2caa90bb84479572a
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2023-05-02 22:43:36 +0500

    added main class

---
+++ src/main/java/com/example/sharnir_bot/SharnirBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharnir_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharnirBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharnirBotApplication.class, args);
+    }
+
+}
